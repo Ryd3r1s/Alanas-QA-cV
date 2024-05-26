da9e2a631c01495245582e795fe72748e785671f
Ryd3r1s <alansulsk@gmail.com>
2024-05-26 20:39:20 +0300

    Add test login, VIN check, and voucher discount validation

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -6,11 +6,117 @@ test("test", async ({ page }) => {
   }
 
   async function acceptCookies() {
-    page.getByRole("button", { name: "Accept all" }).click();
-    await page.getByRole("button", { name: "Close" }).click();
+    await page.getByRole("button", { name: "Accept all" }).click();
   }
 
-  visitHomePage();
+  async function stayinUS() {
+    await page.getByRole("button", { name: "Stay in US" }).click();
+  }
 
-  acceptCookies();
+  async function enterVin() {
+    await page
+      .locator("section")
+      .filter({ hasText: "Learn the story of your" })
+      .getByLabel("Enter VIN numberEnter VIN")
+      .click();
+    await page
+      .locator("section")
+      .filter({ hasText: "Learn the story of your" })
+      .getByLabel("Enter VIN numberEnter VIN")
+      .fill("SALLAAA146A396339");
+  }
+
+  async function getReport() {
+    await page.getByRole("button", { name: "Get report" }).first().click();
+  }
+
+  async function selectbuyingcar() {
+    await page.getByText("Buying a car").click();
+    await page.getByRole("button", { name: "Continue" }).click();
+  }
+
+  async function selectreportpackage() {
+    await page.locator('div').filter({ hasText: /^Check 3 cars-47%\$15\.99\/ reportYou pay \$47\.97\$89\.97$/ }).nth(2).click();
+  }
+
+  async function getreportagain() {
+    await page.getByRole('link', { name: 'Get report' }).click();
+  }
+
+  async function stayinUSagain() {
+    await page.getByRole("link", { name: "Stay in US" }).click();
+  }
+
+  async function emailLoginform() {
+    await page.getByLabel("Your email").click();
+    await page.getByLabel("Your email").fill("alansulsk@gmail.com");
+    await page
+      .locator("label")
+      .filter({ hasText: "I agree to the terms and" })
+      .click();
+    await page.getByRole("button", { name: "Continue" }).click();
+    await page.getByLabel("Your password").click();
+    await page.getByLabel("Your password").fill("Testuoju123");
+    await page.getByRole("button", { name: "Log In", exact: true }).click();
+  }
+
+  await visitHomePage();
+
+  await acceptCookies();
+
+  await stayinUS();
+
+  await enterVin();
+
+  await getReport();
+
+  await selectbuyingcar();
+
+  await selectreportpackage();
+
+  await getreportagain();
+
+  await stayinUSagain();
+
+  await emailLoginform();
+
+  async function getPrice() {
+    const priceElement = await page.getByTestId("Checkout-TotalAmount");
+    const initialPrice = await priceElement.textContent();
+
+    if (!initialPrice) {
+      return 0;
+    }
+
+    return parseFloat(initialPrice.replace("$", "").trim());
+  }
+  const priceBeforeVoucher = await getPrice();
+
+  async function applyVoucher() {
+    await page.getByRole("button", { name: "Add" }).click();
+    await page.getByPlaceholder("Voucher code").click();
+    await page.getByPlaceholder("Voucher code").fill("qahomework");
+    await page.getByRole("button", { name: "Apply" }).click();
+  }
+  await applyVoucher();
+
+  async function validateDiscountPercentage() {
+    const discountValueElement = await page.getByTestId(
+      "Checkout-VoucherRemoveButton"
+    );
+    const discount = await discountValueElement.textContent();
+    const discountPercentage = discount
+      ? parseFloat(discount.replace("%", "").trim()) / -100
+      : 0;
+
+    const priceAfterVoucher = await getPrice();
+
+    const expectedPriceAfterVoucher = (
+      priceBeforeVoucher *
+      (1 - discountPercentage)
+    ).toFixed(2);
+
+    expect(priceAfterVoucher.toFixed(2)).toBe(expectedPriceAfterVoucher);
+  }
+  await validateDiscountPercentage();
 });
